0b9dcd654b13dc31428a55fad9f0f0c6c5b93690
SihaoHu <husihao26@gmail.com>
2023-10-17 18:21:01 -0400

    Initialization

---
+++ Model/gen_seq.py
@@ -0,0 +1,308 @@
+import numpy as np
+import pickle as pkl
+import argparse
+import functools
+import os
+
+parser = argparse.ArgumentParser(description="Data Processing with PyTorch")
+parser.add_argument("--phisher", action='store_true', help="whether to include phisher detection dataset.")
+parser.add_argument("--deanon", action='store_true', help="whether to include de-anonymization dataset.")
+parser.add_argument("--tornado", action='store_true', help="whether to include tornado dataset.")
+parser.add_argument("--data_dir", type=str, default="./Data", help="data directory.")
+parser.add_argument("--dataset", type=str, default=None, help="which dataset to use")
+parser.add_argument("--bizdate", type=str, default=None, help="the date of running experiments.")
+parser.add_argument("--dup", action='store_true', help="whether to do transaction duplication")
+args = parser.parse_args()
+
+print("Duplication:", args.dup)
+
+if args.bizdate is None:
+    raise ValueError("bizdate is required..")
+
+HEADER = 'hash,nonce,block_hash,block_number,transaction_index,from_address,to_address,value,gas,gas_price,input,block_timestamp,max_fee_per_gas,max_priority_fee_per_gas,transaction_type'.split(",")
+
+def cmp_udf(x1, x2):
+    time1 = int(x1[2])
+    time2 = int(x2[2])
+    if time1 < time2:
+        return -1
+    elif time1 > time2:
+        return 1
+    else:
+        return 0
+
+def cmp_udf_reverse(x1, x2):
+    time1 = int(x1[2])
+    time2 = int(x2[2])
+
+    if time1 < time2:
+        return 1
+    elif time1 > time2:
+        return -1
+    else:
+        return 0
+
+def load_data(f_in, f_out):
+    eoa2seq_out = {}
+    error_trans = []
+    while True:
+        trans = f_out.readline()
+        if trans == "":
+            break
+        record = trans.split(",")
+        trans_hash = record[0]
+        block_number = int(record[3])
+        from_address = record[5]
+        to_address = record[6]
+        value = int(record[7]) / (pow(10, 12))
+        gas = int(record[8])
+        gas_price = int(record[9])
+        block_timestamp = int(record[11])
+        if from_address == "" or to_address == "":
+            error_trans.append(trans)
+            continue
+        try:
+            eoa2seq_out[from_address].append([to_address, block_number, block_timestamp, value, "OUT", 1])
+        except:
+            eoa2seq_out[from_address] = [[to_address, block_number, block_timestamp, value, "OUT", 1]]
+
+    eoa2seq_in = {}
+    while True:
+        trans = f_in.readline()
+        if trans == "":
+            break
+        record = trans.split(",")
+        block_number = int(record[3])
+        from_address = record[5]
+        to_address = record[6]
+        value = int(record[7]) / (pow(10, 12))
+        gas = int(record[8])
+        gas_price = int(record[9])
+        block_timestamp = int(record[11])
+        if from_address == "" or to_address == "":
+            error_trans.append(trans)
+            continue
+        try:
+            eoa2seq_in[to_address].append([from_address, block_number, block_timestamp, value, "IN", 1]) # not process trans
+        except:
+            eoa2seq_in[to_address] = [[from_address, block_number, block_timestamp, value, "IN", 1]] # in/out, cnt
+    return eoa2seq_in, eoa2seq_out
+
+def seq_duplicate(eoa2seq_in, eoa2seq_out):
+    eoa2seq_agg_in = {}
+    for eoa in eoa2seq_in.keys():
+        if len(eoa2seq_in[eoa]) >= 10000:
+            continue
+        seq_sorted = sorted(eoa2seq_in[eoa], key=functools.cmp_to_key(cmp_udf))
+        seq_tmp = [e.copy() for e in seq_sorted]
+        for i in range(len(seq_tmp) - 1, 0, -1):
+            l_acc = seq_tmp[i][0]  # latter
+            f_acc = seq_tmp[i - 1][0]  # former
+            l_time = int(seq_tmp[i][2])
+            f_time = int(seq_tmp[i - 1][2])
+            delta_time = l_time - f_time
+            if f_acc != l_acc or delta_time > 86400 * 3:
+                continue
+            # value add
+            seq_tmp[i - 1][3] += seq_tmp[i][3]
+            seq_tmp[i - 1][5] += seq_tmp[i][5]
+            del seq_tmp[i]
+        eoa2seq_agg_in[eoa] = seq_tmp
+
+    eoa2seq_agg_out = {}
+    for eoa in eoa2seq_out.keys():
+        if len(eoa2seq_out[eoa])>=10000:
+            continue
+        seq_sorted = sorted(eoa2seq_out[eoa], key=functools.cmp_to_key(cmp_udf))
+        seq_tmp = [e.copy() for e in seq_sorted]
+        for i in range(len(seq_tmp) - 1, 0, -1):
+            l_acc = seq_tmp[i][0]  # latter
+            f_acc = seq_tmp[i - 1][0]  # former
+            l_time = int(seq_tmp[i][2])
+            f_time = int(seq_tmp[i - 1][2])
+            delta_time = l_time - f_time
+            if f_acc != l_acc or delta_time > 86400 * 3:
+                continue
+            # value add
+            seq_tmp[i - 1][3] += seq_tmp[i][3]
+            seq_tmp[i - 1][5] += seq_tmp[i][5]
+            del seq_tmp[i]
+        eoa2seq_agg_out[eoa] = seq_tmp
+
+    eoa_list = list(eoa2seq_agg_out.keys()) # eoa_list must include eoa account only (i.e., have out transaction at least)
+    eoa2seq_agg = {}
+
+    for eoa in eoa_list:
+        out_seq = eoa2seq_agg_out[eoa]
+        try:
+            in_seq = eoa2seq_agg_in[eoa]
+        except:
+            in_seq = []
+
+        seq_agg = sorted(out_seq + in_seq, key=functools.cmp_to_key(cmp_udf_reverse))
+        cnt_all = 0
+        for trans in seq_agg:
+            cnt_all += trans[5]
+            # if cnt_all >= 20 and cnt_all<=10000:
+            if cnt_all > 2 and cnt_all<=10000:
+                eoa2seq_agg[eoa] = seq_agg
+                break
+
+    return eoa2seq_agg
+
+def seq_generation(eoa2seq_in, eoa2seq_out):
+
+    eoa_list = list(eoa2seq_out.keys()) # eoa_list must include eoa account only (i.e., have out transaction at least)
+    eoa2seq = {}
+    for eoa in eoa_list:
+        out_seq = eoa2seq_out[eoa]
+        try:
+            in_seq = eoa2seq_in[eoa]
+        except:
+            in_seq = []
+        seq_agg = sorted(out_seq + in_seq, key=functools.cmp_to_key(cmp_udf_reverse))
+        cnt_all = 0
+        for trans in seq_agg:
+            cnt_all += 1
+            # if cnt_all >= 5 and cnt_all<=10000:
+            if cnt_all > 2 and cnt_all<=10000:
+                eoa2seq[eoa] = seq_agg
+                break
+
+    return eoa2seq
+
+def feature_bucketization(eoa2seq_agg):
+
+    for eoa in eoa2seq_agg.keys():
+        seq = eoa2seq_agg[eoa]
+        for trans in seq:
+            amount = trans[3]
+            cnt = trans[5]
+
+            if amount == 0:
+                amount_bucket = 1
+            elif amount<= 591:
+                amount_bucket = 2
+            elif amount<= 6195:
+                amount_bucket = 3
+            elif amount <= 21255:
+                amount_bucket = 4
+            elif amount <= 50161:
+                amount_bucket = 5
+            elif amount <= 100120:
+                amount_bucket = 6
+            elif amount <= 208727:
+                amount_bucket = 7
+            elif amount <= 508961:
+                amount_bucket = 8
+            elif amount <= 1360574:
+                amount_bucket = 9
+            elif amount <= 6500000:
+                amount_bucket = 10
+            elif amount <= 143791433950:
+                amount_bucket = 11
+            else:
+                amount_bucket = 12
+
+            trans[3] = amount_bucket
+
+            if cnt == 0:
+                cnt_bucket = 0
+            elif cnt == 1:
+                cnt_bucket = 1
+            elif cnt == 2:
+                cnt_bucket = 2
+            elif cnt == 3:
+                cnt_bucket = 3
+            elif cnt == 4:
+                cnt_bucket = 4
+            elif cnt == 5:
+                cnt_bucket = 5
+            elif cnt == 6:
+                cnt_bucket = 6
+            elif cnt == 7:
+                cnt_bucket = 7
+            elif 8 < cnt <= 10:
+                cnt_bucket = 8
+            elif 10 < cnt <= 20:
+                cnt_bucket = 9
+            else:
+                cnt_bucket = 10
+
+            trans[5] = cnt_bucket
+
+    return eoa2seq_agg
+
+def main():
+
+    f_in = open(os.path.join(args.data_dir, "normal_eoa_transaction_in_slice_1000K.csv"), "r")
+    f_out = open(os.path.join(args.data_dir, "normal_eoa_transaction_out_slice_1000K.csv"), "r")
+    print("Add normal account transactions.")
+
+    eoa2seq_in, eoa2seq_out = load_data(f_in, f_out)
+
+    if args.dup:
+        eoa2seq_agg = seq_duplicate(eoa2seq_in, eoa2seq_out)
+    else:
+        eoa2seq_agg = seq_generation(eoa2seq_in, eoa2seq_out)
+
+    if args.phisher:
+        print("Add phishing..")
+        phisher_f_in = open(os.path.join(args.data_dir, "phisher_transaction_in.csv"), "r")
+        phisher_f_out = open(os.path.join(args.data_dir, "phisher_transaction_out.csv"), "r")
+        phisher_eoa2seq_in, phisher_eoa2seq_out = load_data(phisher_f_in, phisher_f_out)
+
+        if args.dup:
+            phisher_eoa2seq_agg = seq_duplicate(phisher_eoa2seq_in, phisher_eoa2seq_out)
+        else:
+            phisher_eoa2seq_agg = seq_generation(phisher_eoa2seq_in, phisher_eoa2seq_out)
+
+        eoa2seq_agg.update(phisher_eoa2seq_agg)
+
+    if args.deanon:
+        print("Add ENS..")
+        dean_f_in = open(os.path.join(args.data_dir, "dean_trans_in_new.csv"), "r")
+        dean_f_out = open(os.path.join(args.data_dir, "dean_trans_out_new.csv"), "r")
+        dean_eoa2seq_in, dean_eoa2seq_out = load_data(dean_f_in, dean_f_out)
+
+        if args.dup:
+            dean_eoa2seq_agg = seq_duplicate(dean_eoa2seq_in, dean_eoa2seq_out)
+        else:
+            dean_eoa2seq_agg = seq_generation(dean_eoa2seq_in, dean_eoa2seq_out)
+
+        eoa2seq_agg.update(dean_eoa2seq_agg)
+
+    if args.tornado:
+        print("Add tornado...")
+        tornado_in = open(os.path.join(args.data_dir, "tornado_trans_in_removed.csv"), "r")
+        tornado_out = open(os.path.join(args.data_dir, "tornado_trans_out_removed.csv"), "r")
+        tornado_eoa2seq_in, tornado_eoa2seq_out = load_data(tornado_in, tornado_out)
+
+        if args.dup:
+            tornado_eoa2seq_agg = seq_duplicate(tornado_eoa2seq_in, tornado_eoa2seq_out)
+        else:
+            tornado_eoa2seq_agg = seq_generation(tornado_eoa2seq_in, tornado_eoa2seq_out)
+
+        eoa2seq_agg.update(tornado_eoa2seq_agg)
+
+    eoa2seq_agg = feature_bucketization(eoa2seq_agg)
+
+    print("statistics:")
+    length_list = []
+    for eoa in eoa2seq_agg.keys():
+        seq = eoa2seq_agg[eoa]
+        length_list.append(len(seq))
+
+    length_list = np.array(length_list)
+    print("Median:", np.median(length_list))
+    print("Mean:", np.mean(length_list))
+    print("Seq #:", len(length_list))
+
+    os.makedirs("./inter_data", exist_ok=True)
+
+    with open("./inter_data/eoa2seq_" + args.bizdate + ".pkl", "wb") as f:
+        pkl.dump(eoa2seq_agg, f)
+
+
+if __name__ == '__main__':
+    main()
---
+++ README.md
@@ -0,0 +1,184 @@
+# BERT4ETH
+
+This is the PyTorch implementation for the paper [BERT4ETH: A Pre-trained Transformer for Ethereum Fraud Detection](https://dl.acm.org/doi/abs/10.1145/3543507.3583345), accepted by the ACM Web conference (WWW) 2023.
+
+I am still working on :)
+
+## Getting Start
+
+### Requirements
+
+
+### Preprocess dataset 
+
+#### Step 1: Download dataset from Google Drive. 
+* Transaction Dataset:
+* * [Phishing Account](https://drive.google.com/file/d/11UAhLOcffzLyPhdsIqRuFsJNSqNvrNJf/view?usp=sharing)
+
+* * [De-anonymization(ENS)](https://drive.google.com/file/d/1Yveis90jCx-nIA6pUL_4SUezMsVJr8dp/view?usp=sharing)
+
+* * [De-anonymization(Tornado)](https://drive.google.com/file/d/1DMbPSZMSvTYMKUZg3oYKFrjPo2_jeeG4/view?usp=sharing)
+
+* * [Normal Account](https://drive.google.com/file/d/1-htLUymg1UxDrXcI8tslU9wbn0E1vl9_/view?usp=sharing)
+
+* [ERC-20 Log Dataset (all in one)](https://drive.google.com/file/d/1mB2Tf7tMq5ApKKOVdctaTh2UZzzrAVxq/view?usp=sharing)
+
+The master branch hosts the basic BERT4ETH model. If you wish to run the basic BERT4ETH model, there is no need to download the ERC-20 log dataset. Advanced features such as In/out separation and ERC20 log can be found in the old branch.
+
+#### Step 2: Unzip dataset under the directory of "BERT4ETH/Data/" 
+
+```sh
+cd BERT4ETH/Data; # Labels are already included
+unzip ...;
+``` 
+
+#### Step 3: Transaction Sequence Generation
+
+```sh
+cd Model;
+python gen_seq.py --bizdate=bert4eth_exp
+```
+
+
+### Pre-training
+
+<!-- 
+#### Step 0: Model Configuration
+
+The configuration file is "Model/bert_config.json"
+```
+{
+  "attention_probs_dropout_prob": 0.2,
+  "hidden_act": "gelu",
+  "hidden_dropout_prob": 0.2,
+  "hidden_size": 64,
+  "initializer_range": 0.02,
+  "intermediate_size": 64,
+  "max_position_embeddings": 50,
+  "num_attention_heads": 2,
+  "num_hidden_layers": 8,
+  "type_vocab_size": 2,
+  "vocab_size": 3000000
+}
+```
+-->
+
+#### Step 1: Pre-training Data Generation from Sequence 
+
+[//]: # (&#40;Masking, I/O separation and ERC20 log&#41;)
+
+```sh
+python gen_pretrain_data.py --bizdate=bert4eth_exp  \ 
+                            --max_seq_length=100  \
+                            --dupe_factor=10 \
+                            --masked_lm_prob=0.8 
+```
+
+
+#### Step 2: Pre-train BERT4ETH 
+
+```sh
+python run_pretrain.py --bizdate=bert4eth_exp \
+                       --max_seq_length=100 \
+                       --epoch=5 \
+                       --batch_size=256 \
+                       --learning_rate=1e-4 \
+                       --num_train_steps=1000000 \
+                       --save_checkpoints_steps=8000 \
+                       --neg_strategy=zip \
+                       --neg_sample_num=5000 \ 
+                       --neg_share=True \ 
+                       --checkpointDir=bert4eth_exp 
+```
+
+
+| Parameter                | Description                                                                        |
+|--------------------------|------------------------------------------------------------------------------------|
+| `bizdate`                | The signature for this experiment run.                                             |
+| `max_seq_length`         | The maximum length of BERT4ETH.                                                    |
+| `masked_lm_prob`         | The probability of masking an address.                                             |
+| `epochs`                 | Number of training epochs, default = `5`.                                          |
+| `batch_size`             | Batch size, default = `256`.                                                       |
+| `learning_rate`          | Learning rate for the optimizer (Adam), default = `1e-4`.                          |
+| `num_train_steps`        | The maximum number of training steps, default = `1000000`,                         |
+| `save_checkpoints_steps` | The parameter controlling the step of saving checkpoints, default = `8000`.        |
+| `neg_strategy`           | Strategy for negative sampling, default `zip`, options (`uniform`, `zip`, `freq`). |
+| `neg_share`              | Whether enable in-batch sharing strategy, default = `True`.                        |
+| `neg_sample_num`         | The negative sampling number for one batch, default = `5000`.                      |
+| `checkpointDir`          | Specify the directory to save the checkpoints.                                     |
+
+
+#### Step 3: Output Representation
+
+```sh
+python output_embed.py --bizdate=bert4eth_exp \
+                       --init_checkpoint=bert4eth_exp/model_104000 \
+                       --max_seq_length=100 \
+                       --neg_sample_num=5000 \
+                       --neg_strategy=zip \
+                       --neg_share=True
+```
+
+I have generated a version of [embedding file](https://drive.google.com/file/d/1mQgO1LalWhjeR064VhPRmkLl_ztSTroW/view?usp=sharing), you can unzip it under the directory of "Model/inter_data/" and test the results.
+
+### Testing on output account representation
+
+#### Phishing Account Detection
+```sh
+python run_phishing_detection.py --init_checkpoint=bert4eth_exp/model_104000 # Random Forest (RF)
+
+python run_phishing_detection_dnn.py --init_checkpoint=bert4eth_exp/model_104000 # DNN, better than RF
+```
+
+#### De-anonymization (ENS dataset)
+
+```sh
+python run_dean_ENS.py --metric=euclidean \
+                       --init_checkpoint=bert4eth_exp/model_104000
+```
+
+<!-- 
+#### De-anonymization (Tornado Cash)
+
+```sh
+
+python run_dean_Tornado.py --metric=euclidean \
+
+                           --init_checkpoint=bert4eth_exp/model_104000
+
+```
+-->
+
+### Fine-tuning for phishing account detection
+```sh
+
+python gen_finetune_phisher_data.py --bizdate=bert4eth_exp \ 
+                                    --max_seq_length=100 
+```
+
+```sh
+python run_finetune_phisher.py --init_checkpoint=bert4eth_exp/model_104000 \
+                               --bizdate=bert4eth_exp \ 
+                               --max_seq_length=100 \ 
+                               --checkpointDir=tmp
+```
+
+-----
+## Citation
+
+If you find this repository useful, please give us a star and cite our paper : ) Thank you!
+```
+@inproceedings{hu2023bert4eth,
+  title={BERT4ETH: A Pre-trained Transformer for Ethereum Fraud Detection},
+  author={Hu, Sihao and Zhang, Zhen and Luo, Bingqiao and Lu, Shengliang and He, Bingsheng and Liu, Ling},
+  booktitle={Proceedings of the ACM Web Conference 2023},
+  pages={2189--2197},
+  year={2023}
+}
+```
+
+-----
+## Q&A
+
+If you have any questions, you can either open an issue or contact me (sihaohu@gatech.edu), and I will reply as soon as I see the issue or email.
+
