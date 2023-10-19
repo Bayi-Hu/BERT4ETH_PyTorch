3912a9eeffd933d4cccccc2f9b3536420d94d40e
SihaoHu <husihao26@gmail.com>
2023-10-19 13:14:05 -0400

    Update generate pretrain data

--- Model/gen_pretrain_data.py
+++ Model/gen_pretrain_data.py
@@ -1,40 +1,37 @@
-import pandas as pd
+import torch
+import torch.utils.data as data_utils
+
 import numpy as np
 import pickle as pkl
 from tqdm import tqdm
 import collections
 import functools
 import random
-import tensorflow.compat.v1 as tf
-
-tf.disable_v2_behavior()
+import argparse
 
 import six
 import time
 import math
 from vocab import FreqVocab
 
-tf.logging.set_verbosity(tf.logging.INFO)
-
 random_seed = 12345
 rng = random.Random(random_seed)
 
-short_seq_prob = 0  # Probability of creating sequences which are shorter than the maximum length。
-flags = tf.flags
-FLAGS = flags.FLAGS
+parser = argparse.ArgumentParser(description="Data Processing with PyTorch")
+parser.add_argument('--pool_size', type=int, default=10, help='multiprocesses pool size.')
+parser.add_argument('--max_seq_length', type=int, default=100, help='max sequence length.')
+parser.add_argument('--masked_lm_prob', type=float, default=0.8, help='Masked LM probability.')
+parser.add_argument('--mask_prob', type=float, default=1.0, help='mask probability')
+parser.add_argument('--do_eval', action='store_true', help='Whether to do evaluation.')
+parser.add_argument('--do_embed', action='store_true', default=True, help='Whether to do embedding.')
+parser.add_argument('--dupe_factor', type=int, default=10, help='Number of times to duplicate the input data (with different masks).')
+parser.add_argument('--data_dir', type=str, default='./inter_data/', help='data dir.')
+parser.add_argument('--vocab_filename', type=str, default='vocab', help='vocab filename')
+parser.add_argument('--bizdate', type=str, default=None, help='the signature of running experiments')
 
-flags.DEFINE_integer("pool_size", 10, "multiprocesses pool size.")
-flags.DEFINE_integer("max_seq_length", 100, "max sequence length.")
-flags.DEFINE_float("masked_lm_prob", 0.8, "Masked LM probability.")
-flags.DEFINE_float("mask_prob", 1.0, "mask probabaility")
-flags.DEFINE_bool("do_eval", False, "")
-flags.DEFINE_bool("do_embed", True, "")
-flags.DEFINE_integer("dupe_factor", 10, "Number of times to duplicate the input data (with different masks).")
-flags.DEFINE_string("data_dir", './inter_data/', "data dir.")
-flags.DEFINE_string("vocab_filename", "vocab", "vocab filename")
-flags.DEFINE_string("bizdate", None, "the signature of running experiments")
+args = parser.parse_args()
 
-if FLAGS.bizdate is None:
+if args.bizdate is None:
     raise ValueError("bizdate is required.")
 
 HEADER = 'hash,nonce,block_hash,block_number,transaction_index,from_address,to_address,value,gas,gas_price,input,block_timestamp,max_fee_per_gas,max_priority_fee_per_gas,transaction_type'.split(
@@ -43,25 +40,55 @@ HEADER = 'hash,nonce,block_hash,block_number,transaction_index,from_address,to_a
 MaskedLmInstance = collections.namedtuple("MaskedLmInstance",
                                           ["index", "label"])
 
-MAX_PREDICTIONS_PER_SEQ = math.ceil(FLAGS.max_seq_length * FLAGS.masked_lm_prob)
-SLIDING_STEP = round(FLAGS.max_seq_length * 0.6)
+MAX_PREDICTIONS_PER_SEQ = math.ceil(args.max_seq_length * args.masked_lm_prob)
+SLIDING_STEP = round(args.max_seq_length * 0.6)
 
-print("MAX_SEQUENCE_LENGTH:", FLAGS.max_seq_length)
+print("MAX_SEQUENCE_LENGTH:", args.max_seq_length)
 print("MAX_PREDICTIONS_PER_SEQ:", MAX_PREDICTIONS_PER_SEQ)
 print("SLIDING_STEP:", SLIDING_STEP)
 
-class TrainingInstance(object):
-    """A single training instance (sentence pair)."""
+class BertTrainDataset(data_utils.Dataset):
+    def __init__(self, seq_list, mask_prob, mask_token, max_predictions_per_seq):
+        self.seq_list = seq_list
+        self.mask_prob = mask_prob
+        self.mask_token = mask_token
+        self.max_predictions_per_seq = max_predictions_per_seq
+        self.rng = rng
+    def __getitem__(self, index):
 
-    def __init__(self, address, tokens, masked_lm_positions, masked_lm_labels):
+        # only one index as input
+        tranxs = self.seq_list[index]
+        address = tranxs[0][0]
+        cand_indexes = []
+        for (i, token) in enumerate(tranxs):
+            cand_indexes.append(i)
 
-        self.address = [address]
-        self.tokens = list(map(lambda x: x[0], tokens))
-        self.block_timestamps = list(map(lambda x: x[2], tokens))
-        self.values = list(map(lambda x: x[3], tokens))
+        rng.shuffle(cand_indexes)
+        num_to_predict = min(self.max_predictions_per_seq,
+                         max(1, int(len(tranxs) * self.masked_lm_prob)))
 
-        def map_io_flag(token):
-            flag = token[4]
+        masked_lms = []
+        covered_indexes = set()
+        labels = [0 for i in range(len(tranxs))] # labels = 0 denotes not masked.
+
+        for index in cand_indexes:
+            if len(masked_lms) >= num_to_predict:
+                break
+            if index in covered_indexes:
+                continue
+            covered_indexes.add(index)
+            labels[index] = tranxs[index][0]
+            tranxs[index][0] = "[MASK]"
+
+        # MAP discrete feature to int
+
+        address = [address]
+        tokens = list(map(lambda x: x[0], tranxs))
+        block_timestamps = list(map(lambda x: x[2], tranxs))
+        values = list(map(lambda x: x[3], tranxs))
+
+        def map_io_flag(tranxs):
+            flag = tranxs[4]
             if flag == "OUT":
                 return 1
             elif flag == "IN":
@@ -69,59 +96,70 @@ class TrainingInstance(object):
             else:
                 return 0
 
-        self.io_flags = list(map(map_io_flag, tokens))
-        self.cnts = list(map(lambda x: x[5], tokens))
-        self.masked_lm_positions = masked_lm_positions
-        self.masked_lm_labels = masked_lm_labels
+        io_flags = list(map(map_io_flag, tranxs))
+        cnts = list(map(lambda x: x[5], tranxs))
 
-    def __str__(self):
-        s = "address: %s\n" % (self.address[0])
-        s += "tokens: %s\n" % (
-            " ".join([printable_text(x) for x in self.tokens]))
-        s += "masked_lm_positions: %s\n" % (
-            " ".join([str(x) for x in self.masked_lm_positions]))
-        s += "masked_lm_labels: %s\n" % (
-            " ".join([printable_text(x) for x in self.masked_lm_labels]))
-        s += "\n"
-        return s
-
-    def __repr__(self):
-        return self.__str__()
+        return torch.LongTensor(tokens), \
+               torch.LongTensor(block_timestamps), \
+               torch.LongTensor(values), \
+               torch.LongTensor(io_flags), \
+               torch.LongTensor(cnts), \
+               torch.LongTensor(labels)
 
 
-def printable_text(text):
-    """Returns text encoded in a way suitable for print or `tf.logging`."""
+def gen_samples(sequences,
+                dupe_factor,
+                masked_lm_prob,
+                max_predictions_per_seq,
+                pool_size,
+                rng):
+    instances = []
+    # create train
+    for step in range(dupe_factor):
+        start = time.time()
+        for tokens in sequences:
+            (address, tokens, masked_lm_positions,
+             masked_lm_labels) = create_masked_lm_predictions(
+                tokens, masked_lm_prob, max_predictions_per_seq, rng)
+            instance = TrainingInstance(
+                address=address,
+                tokens=tokens,
+                masked_lm_positions=masked_lm_positions,
+                masked_lm_labels=masked_lm_labels)
+            instances.append(instance)
+        end = time.time()
+        cost = end - start
+        print("step=%d, time=%.2f" % (step, cost))
+    print("=======Finish========")
+    return instances
 
-    # These functions want `str` for both Python2 and Python3, but in one case
-    # it's a Unicode string and in the other it's a byte string.
-    if six.PY3:
-        if isinstance(text, str):
-            return text
-        elif isinstance(text, bytes):
-            return text.decode("utf-8", "ignore")
-        else:
-            raise ValueError("Unsupported string type: %s" % (type(text)))
-    elif six.PY2:
-        if isinstance(text, str):
-            return text
-        elif isinstance(text, unicode):
-            return text.encode("utf-8")
-        else:
-            raise ValueError("Unsupported string type: %s" % (type(text)))
-    else:
-        raise ValueError("Not running on Python2 or Python 3?")
+def create_embedding_predictions(tokens):
+    """Creates the predictions for the masked LM objective."""
+    address = tokens[0][0]
+    output_tokens = tokens
+    masked_lm_positions = []
+    masked_lm_labels = []
+    return (address, output_tokens, masked_lm_positions, masked_lm_labels)
 
 
-def create_int_feature(values):
-    feature = tf.train.Feature(
-        int64_list=tf.train.Int64List(value=list(values)))
-    return feature
+def gen_embedding_samples(sequences):
+    instances = []
+    # create train
+    start = time.time()
+    for tokens in sequences:
+        (address, tokens, masked_lm_positions,
+         masked_lm_labels) = create_embedding_predictions(tokens)
+        instance = TrainingInstance(
+            address=address,
+            tokens=tokens,
+            masked_lm_positions=masked_lm_positions,
+            masked_lm_labels=masked_lm_labels)
+        instances.append(instance)
 
-
-def create_float_feature(values):
-    feature = tf.train.Feature(
-        float_list=tf.train.FloatList(value=list(values)))
-    return feature
+    end = time.time()
+    print("=======Finish========")
+    print("cost time:%.2f" % (end - start))
+    return instances
 
 
 def convert_timestamp_to_position(block_timestamps):
@@ -136,99 +174,6 @@ def convert_timestamp_to_position(block_timestamps):
             idx += 1
         position.append(idx)
     return position
-
-
-def write_instance_to_example_files(instances, max_seq_length,
-                                    max_predictions_per_seq, vocab,
-                                    output_files):
-    """Create TF example files from `TrainingInstance`s."""
-    writers = []
-    for output_file in output_files:
-        writers.append(tf.python_io.TFRecordWriter(output_file))
-
-    writer_index = 0
-    total_written = 0
-
-    for inst_index in tqdm(range(len(instances))):
-        instance = instances[inst_index]
-        input_ids = vocab.convert_tokens_to_ids(instance.tokens)
-        address = vocab.convert_tokens_to_ids(instance.address)
-        counts = instance.cnts
-        block_timestamps = instance.block_timestamps
-        values = instance.values
-        io_flags = instance.io_flags
-        positions = convert_timestamp_to_position(block_timestamps)
-
-        input_mask = [1] * len(input_ids)
-        assert len(input_ids) <= max_seq_length
-        assert len(counts) <= max_seq_length
-        assert len(values) <= max_seq_length
-        assert len(io_flags) <= max_seq_length
-        assert len(positions) <= max_seq_length
-
-        input_ids += [0] * (max_seq_length - len(input_ids))
-        counts += [0] * (max_seq_length - len(counts))
-        values += [0] * (max_seq_length - len(values))
-        io_flags += [0] * (max_seq_length - len(io_flags))
-        positions += [0] * (max_seq_length - len(positions))
-        input_mask += [0] * (max_seq_length - len(input_mask))
-
-        assert len(input_ids) == max_seq_length
-        assert len(counts) == max_seq_length
-        assert len(values) == max_seq_length
-        assert len(io_flags) == max_seq_length
-        assert len(positions) == max_seq_length
-        assert len(input_mask) == max_seq_length
-
-        masked_lm_positions = list(instance.masked_lm_positions)
-        masked_lm_ids = vocab.convert_tokens_to_ids(instance.masked_lm_labels)
-        masked_lm_weights = [1.0] * len(masked_lm_ids)
-
-        masked_lm_positions += [0] * (max_predictions_per_seq - len(masked_lm_positions))
-        masked_lm_ids += [0] * (max_predictions_per_seq - len(masked_lm_ids))
-        masked_lm_weights += [0.0] * (max_predictions_per_seq - len(masked_lm_weights))
-
-        features = collections.OrderedDict()
-        features["address"] = create_int_feature(address)
-        features["input_ids"] = create_int_feature(input_ids)
-        features["input_positions"] = create_int_feature(positions)
-        features["input_counts"] = create_int_feature(counts)
-        features["input_io_flags"] = create_int_feature(io_flags)
-        features["input_values"] = create_int_feature(values)
-
-        features["input_mask"] = create_int_feature(input_mask)
-        features["masked_lm_positions"] = create_int_feature(masked_lm_positions)
-        features["masked_lm_ids"] = create_int_feature(masked_lm_ids)
-        features["masked_lm_weights"] = create_float_feature(masked_lm_weights)
-
-        tf_example = tf.train.Example(
-            features=tf.train.Features(feature=features))
-
-        writers[writer_index].write(tf_example.SerializeToString())
-        writer_index = (writer_index + 1) % len(writers)
-
-        total_written += 1
-
-        if inst_index < 3:
-            tf.logging.info("*** Example ***")
-            tf.logging.info("tokens: %s" % " ".join(
-                [printable_text(x) for x in instance.tokens]))
-
-            for feature_name in features.keys():
-                feature = features[feature_name]
-                values = []
-                if feature.int64_list.value:
-                    values = feature.int64_list.value
-                elif feature.float_list.value:
-                    values = feature.float_list.value
-                tf.logging.info("%s: %s" % (feature_name,
-                                            " ".join([str(x)
-                                                      for x in values])))
-
-    for writer in writers:
-        writer.close()
-
-    tf.logging.info("Wrote %d total instances", total_written)
 
 
 def cmp_udf_reverse(x1, x2):
@@ -246,7 +191,7 @@ def cmp_udf_reverse(x1, x2):
 def main():
     vocab = FreqVocab()
     print("===========Load Sequence===========")
-    with open(FLAGS.data_dir + "eoa2seq_" + FLAGS.bizdate + ".pkl", "rb") as f:
+    with open(args.data_dir + "eoa2seq_" + args.bizdate + ".pkl", "rb") as f:
         eoa2seq = pkl.load(f)
 
     print("number of target user account:", len(eoa2seq))
@@ -256,7 +201,7 @@ def main():
 
     # save vocab
     print("token_size:{}".format(len(vocab.vocab_words)))
-    vocab_file_name = FLAGS.data_dir + FLAGS.vocab_filename + "." + FLAGS.bizdate
+    vocab_file_name = args.data_dir + args.vocab_filename + "." + args.bizdate
     print('vocab pickle file: ' + vocab_file_name)
     with open(vocab_file_name, 'wb') as output_file:
         pkl.dump(vocab, output_file, protocol=2)
@@ -273,7 +218,7 @@ def main():
     print("Seq num:", len(length_list))
 
     # clip
-    max_num_tokens = FLAGS.max_seq_length - 1
+    max_num_tokens = args.max_seq_length - 1
     seqs = []
     idx = 0
     for eoa, seq in eoa2seq.items():
@@ -298,59 +243,27 @@ def main():
                     seqs[idx] += seq[i:i + max_num_tokens]
                     idx += 1
 
-    if FLAGS.do_embed:
-        print("===========Generate Embedding Samples==========")
-        write_instance = gen_embedding_samples(seqs)
-        output_filename = FLAGS.data_dir + "embed.tfrecord" + "." + FLAGS.bizdate
-        tf.logging.info("*** Writing to output embedding files ***")
-        tf.logging.info("  %s", output_filename)
-
-        write_instance_to_example_files(write_instance, FLAGS.max_seq_length,
-                                        MAX_PREDICTIONS_PER_SEQ, vocab,
-                                        [output_filename])
-
     seqs = np.random.permutation(seqs)
 
-    if FLAGS.do_eval:  # select 20% for testing
-        print("========Generate Evaluation Samples========")
-        eval_seqs = seqs[:round(len(seqs) * 0.2)]
-        seqs = seqs[round(len(seqs) * 0.2):]
+    # start from here
 
-        eval_normal_instances = gen_samples(eval_seqs,
-                                            dupe_factor=FLAGS.dupe_factor,
-                                            masked_lm_prob=FLAGS.masked_lm_prob,
-                                            max_predictions_per_seq=MAX_PREDICTIONS_PER_SEQ,
-                                            pool_size=FLAGS.pool_size,
-                                            rng=rng)
 
-        eval_write_instance = eval_normal_instances
-        rng.shuffle(eval_write_instance)
-        eval_output_filename = FLAGS.data_dir + "test.tfrecord" + "." + FLAGS.bizdate
-        tf.logging.info("*** Writing to Testing files ***")
-        tf.logging.info("  %s", eval_output_filename)
 
-        write_instance_to_example_files(eval_write_instance, FLAGS.max_seq_length,
-                                        MAX_PREDICTIONS_PER_SEQ, vocab,
-                                        [eval_output_filename])
+
+
 
     print("========Generate Training Samples========")
     normal_instances = gen_samples(seqs,
-                                   dupe_factor=FLAGS.dupe_factor,
-                                   masked_lm_prob=FLAGS.masked_lm_prob,
+                                   dupe_factor=args.dupe_factor,
+                                   masked_lm_prob=args.masked_lm_prob,
                                    max_predictions_per_seq=MAX_PREDICTIONS_PER_SEQ,
-                                   pool_size=FLAGS.pool_size,
+                                   pool_size=args.pool_size,
                                    rng=rng)
 
     write_instance = normal_instances
     rng.shuffle(write_instance)
 
-    output_filename = FLAGS.data_dir + "train.tfrecord" + "." + FLAGS.bizdate
-    tf.logging.info("*** Writing to Training files ***")
-    tf.logging.info("  %s", output_filename)
-
-    write_instance_to_example_files(write_instance, FLAGS.max_seq_length,
-                                    MAX_PREDICTIONS_PER_SEQ, vocab,
-                                    [output_filename])
+    output_filename = args.data_dir + "train.tfrecord" + "." + args.bizdate
 
     return
 
