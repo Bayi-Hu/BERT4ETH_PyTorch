c9ec7fa5b4e9db1542913451acac5243b85a5f49
SihaoHu <husihao26@gmail.com>
2023-11-12 21:17:42 -0500

    Add git ignore

---
+++ src/modeling.py
@@ -0,0 +1,111 @@
+import torch.nn as nn
+from .gelu import GELU
+from .layer_norm import LayerNorm
+import torch.nn.functional as F
+
+
+class PositionwiseFeedForward(nn.Module):
+    "Implements FFN equation."
+
+    def __init__(self, d_model, d_ff, dropout=0.1):
+        super(PositionwiseFeedForward, self).__init__()
+        self.w_1 = nn.Linear(d_model, d_ff)
+        self.w_2 = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(dropout)
+        self.activation = GELU()
+
+    def forward(self, x):
+        return self.w_2(self.dropout(self.activation(self.w_1(x))))
+
+class GELU(nn.Module):
+    """
+    Paper Section 3.4, last paragraph notice that BERT used the GELU instead of RELU
+    """
+
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(math.sqrt(2 / math.pi) * (x + 0.044715 * torch.pow(x, 3))))
+
+
+class LayerNorm(nn.Module):
+    "Construct a layernorm module (See citation for details)."
+
+    def __init__(self, features, eps=1e-6):
+        super(LayerNorm, self).__init__()
+        self.a_2 = nn.Parameter(torch.ones(features))
+        self.b_2 = nn.Parameter(torch.zeros(features))
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
+
+
+class SublayerConnection(nn.Module):
+    """
+    A residual connection followed by a layer norm.
+    Note for code simplicity the norm is first as opposed to last.
+    """
+
+    def __init__(self, size, dropout):
+        super(SublayerConnection, self).__init__()
+        self.norm = LayerNorm(size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, sublayer):
+        "Apply residual connection to any sublayer with the same size."
+        return x + self.dropout(sublayer(self.norm(x)))
+
+class Attention(nn.Module):
+    """
+    Compute 'Scaled Dot Product Attention
+    """
+
+    def forward(self, query, key, value, mask=None, dropout=None):
+        scores = torch.matmul(query, key.transpose(-2, -1)) \
+                 / math.sqrt(query.size(-1))
+
+        if mask is not None:
+            scores = scores.masked_fill(mask == 0, -1e9)
+
+        p_attn = F.softmax(scores, dim=-1)
+
+        if dropout is not None:
+            p_attn = dropout(p_attn)
+
+        return torch.matmul(p_attn, value), p_attn
+
+
+class MultiHeadedAttention(nn.Module):
+    """
+    Take in model size and number of heads.
+    """
+
+    def __init__(self, h, d_model, dropout=0.1):
+        super().__init__()
+        assert d_model % h == 0
+
+        # We assume d_v always equals d_k
+        self.d_k = d_model // h
+        self.h = h
+
+        self.linear_layers = nn.ModuleList([nn.Linear(d_model, d_model) for _ in range(3)])
+        self.output_linear = nn.Linear(d_model, d_model)
+        self.attention = Attention()
+
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, query, key, value, mask=None):
+        batch_size = query.size(0)
+
+        # 1) Do all the linear projections in batch from d_model => h x d_k
+        query, key, value = [l(x).view(batch_size, -1, self.h, self.d_k).transpose(1, 2)
+                             for l, x in zip(self.linear_layers, (query, key, value))]
+
+        # 2) Apply attention on all the projected vectors in batch.
+        x, attn = self.attention(query, key, value, mask=mask, dropout=self.dropout)
+
+        # 3) "Concat" using a view and apply a final linear.
+        x = x.transpose(1, 2).contiguous().view(batch_size, -1, self.h * self.d_k)
+
+        return self.output_linear(x)
---
+++ src/my_options.py
@@ -0,0 +1,98 @@
+from templates import set_template
+
+import argparse
+
+
+parser = argparse.ArgumentParser(description='RecPlay')
+
+################
+# Top Level
+################
+parser.add_argument('--mode', type=str, default='train', choices=['train'])
+parser.add_argument('--template', type=str, default=None)
+
+################
+# Test
+################
+parser.add_argument('--test_model_path', type=str, default=None)
+
+################
+# Dataset
+################
+parser.add_argument('--dataset_code', type=str, default='ml-1m')
+parser.add_argument('--min_rating', type=int, default=4, help='Only keep ratings greater than equal to this value')
+parser.add_argument('--min_uc', type=int, default=5, help='Only keep users with more than min_uc ratings')
+parser.add_argument('--min_sc', type=int, default=0, help='Only keep items with more than min_sc ratings')
+parser.add_argument('--split', type=str, default='leave_one_out', help='How to split the datasets')
+parser.add_argument('--dataset_split_seed', type=int, default=98765)
+parser.add_argument('--eval_set_size', type=int, default=500, 
+                    help='Size of val and test set. 500 for ML-1m and 10000 for ML-20m recommended')
+
+################
+# Dataloader
+################
+parser.add_argument('--dataloader_code', type=str, default='bert')
+parser.add_argument('--dataloader_random_seed', type=float, default=0.0)
+parser.add_argument('--train_batch_size', type=int, default=64)
+
+################
+# NegativeSampler
+################
+parser.add_argument('--train_negative_sampler_code', type=str, default='random', choices=['popular', 'random'],
+                    help='Method to sample negative items for training. Not used in bert')
+parser.add_argument('--train_negative_sample_size', type=int, default=100)
+parser.add_argument('--train_negative_sampling_seed', type=int, default=None)
+
+################
+# Trainer
+################
+parser.add_argument('--trainer_code', type=str, default='bert')
+# device #
+parser.add_argument('--device', type=str, default='cpu', choices=['cpu', 'cuda'])
+parser.add_argument('--num_gpu', type=int, default=1)
+parser.add_argument('--device_idx', type=str, default='0')
+# optimizer #
+parser.add_argument('--optimizer', type=str, default='Adam', choices=['SGD', 'Adam'])
+parser.add_argument('--lr', type=float, default=0.001, help='Learning rate')
+parser.add_argument('--weight_decay', type=float, default=0, help='l2 regularization')
+parser.add_argument('--momentum', type=float, default=None, help='SGD momentum')
+# lr scheduler #
+parser.add_argument('--decay_step', type=int, default=15, help='Decay step for StepLR')
+parser.add_argument('--gamma', type=float, default=0.1, help='Gamma for StepLR')
+# epochs #
+parser.add_argument('--num_epochs', type=int, default=100, help='Number of epochs for training')
+# logger #
+parser.add_argument('--log_period_as_iter', type=int, default=12800)
+# evaluation #
+parser.add_argument('--metric_ks', nargs='+', type=int, default=[10, 20, 50], help='ks for Metric@k')
+parser.add_argument('--best_metric', type=str, default='NDCG@10', help='Metric for determining the best model')
+# Finding optimal beta for VAE #
+parser.add_argument('--find_best_beta', type=bool, default=False, 
+                    help='If set True, the trainer will anneal beta all the way up to 1.0 and find the best beta')
+parser.add_argument('--total_anneal_steps', type=int, default=2000, help='The step number when beta reaches 1.0')
+parser.add_argument('--anneal_cap', type=float, default=0.2, help='Upper limit of increasing beta. Set this as the best beta found')
+
+################
+# Model
+################
+parser.add_argument('--model_code', type=str, default='bert')
+parser.add_argument('--model_init_seed', type=int, default=None)
+# BERT #
+parser.add_argument('--bert_max_len', type=int, default=None, help='Length of sequence for bert')
+parser.add_argument('--bert_num_items', type=int, default=None, help='Number of total items')
+parser.add_argument('--bert_hidden_units', type=int, default=None, help='Size of hidden vectors (d_model)')
+parser.add_argument('--bert_num_blocks', type=int, default=None, help='Number of transformer layers')
+parser.add_argument('--bert_num_heads', type=int, default=None, help='Number of heads for multi-attention')
+parser.add_argument('--bert_dropout', type=float, default=None, help='Dropout probability to use throughout the model')
+parser.add_argument('--bert_mask_prob', type=float, default=None, help='Probability for masking items in the training sequence')
+
+################
+# Experiment
+################
+parser.add_argument('--experiment_dir', type=str, default='experiments')
+parser.add_argument('--experiment_description', type=str, default='test')
+
+
+################
+args = parser.parse_args()
+set_template(args)
